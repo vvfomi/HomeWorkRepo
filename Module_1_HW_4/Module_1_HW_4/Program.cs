e1f38d68af0112c2654c6833e4a93d0a04023c2e
Yevhen Nesterenko <yevhen.nesterenko@dataart.com>
2023-02-11 00:24:57 +0200

    Created arrays for even and odd numbers

--- Module_1_HW_4/Module_1_HW_4/Program.cs
+++ Module_1_HW_4/Module_1_HW_4/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace Module_1_HW_4
+﻿namespace Module_1_HW_4;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Enter array's size: ");
+
+        int[] numbers = new int[int.Parse(Console.ReadLine())];
+
+        for (int i = 0; i < numbers.Length; i++)
         {
-            Console.WriteLine("Hello, World!");
+            numbers[i] = new Random().Next(1, 26);
+            Console.WriteLine(numbers[i]);
+        }
+
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+
+        }
+
+        int[] evenArray = new int[even];
+        int[] oddArray = new int[odd];
+
+        int evenArrayIndex = 0;
+        int oddArrayIndex = 0; 
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i]%2==0)
+            {
+                evenArray[evenArrayIndex] = numbers[i];
+                evenArrayIndex++;
+            }
+            else
+            {
+                oddArray[oddArrayIndex] = numbers[i];
+                oddArrayIndex++;
+            }
         }
     }
 }
