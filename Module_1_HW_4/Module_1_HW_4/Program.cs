6c335591c40185584a5efc99d70c64adbf3433c2
Yevhen Nesterenko <yevhen.nesterenko@dataart.com>
2023-02-10 23:36:07 +0200

    Created new empty project for homework

---
+++ Module_1_HW_4/Module_1_HW_4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module_1_HW_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
