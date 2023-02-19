fd1db152a1a18a1b48627e6c25256074f21ba2ce
Vlada Fomina <vladafomina190901@gmail.com>
2023-02-19 17:38:03 +0200

    Created Singleton Logger

---
+++ Module-2-HW-1/Module2_HW1/Module2_HW1/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Module2_HW1;
+
+public class Logger
+{
+    private static Logger _instance;
+    private StringBuilder _logs;
+
+    private Logger()
+    {
+        _logs = new StringBuilder();
+    }
+
+    public static Logger GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Logger();
+        }
+        return _instance;
+    }
+
+    public void LogRecording(LogEnum logEnum, string message)
+    {
+        string log = $"{DateTime.Now}: {logEnum}: {message}";
+        Console.WriteLine(log);
+
+        _logs.AppendLine(log);
+        File.WriteAllText("log.txt", _logs.ToString());
+    }
+
+    public string GetAllLogs()
+    {
+        return _logs.ToString();
+    }
+}
+
---
+++ Module-2-HW-1/Module2_HW1/Module2_HW1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Module2_HW1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var logger = Logger.GetInstance();
+
+        logger.LogRecording(LogEnum.Info, "Program was started. ");
+        Thread.Sleep(1000);
+        var logger2 = Logger.GetInstance();
+
+        logger2.LogRecording(LogEnum.Warning, "Testing warning log.");
+        Thread.Sleep(1000);
+        var logger3 = Logger.GetInstance();
+
+        logger3.LogRecording(LogEnum.Error, "Oops, something went wrong! ");
+
+        var logs = logger.GetAllLogs();
+
+
+        Console.WriteLine($"\nAll logs:\n{logs}");
+    }
+}
