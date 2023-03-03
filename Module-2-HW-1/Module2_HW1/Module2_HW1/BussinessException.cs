5a4a318656947745dee8c2c8c36d5c50d511d3d4
Vlada Fomina <vladafomina190901@gmail.com>
2023-03-03 17:15:19 +0200

    Lection 3, completed task

---
+++ Module-2-HW-1/Module2_HW1/Module2_HW1/BussinessException.cs
@@ -0,0 +1,10 @@
+﻿namespace Module2_HW1;
+
+public class BussinessException : Exception
+{
+    public BussinessException(string message) : base(message)
+    {
+
+    }
+
+}
---
+++ Module-2-HW-1/Module2_HW1/Module2_HW1/FileService.cs
@@ -0,0 +1,24 @@
+﻿namespace Module2_HW1;
+public class FileService
+{
+    public void CreateFile(string folderName, string fileName, string text)
+    {
+        CheckOldestFile(folderName);
+        string filePath = folderName + fileName; 
+        File.WriteAllText(filePath, text);
+    }
+
+    public void CheckOldestFile(string folderName)
+    {
+        var fileInfos = new DirectoryInfo(folderName).GetFileSystemInfos();
+
+        if (fileInfos.Length < 3)
+        {
+            return;
+        }
+        var sortedFileInfos = fileInfos.OrderBy(fileInfo => fileInfo.CreationTime ).ToArray();
+        var oldestFile = sortedFileInfos[0];
+        oldestFile.Delete();
+    }
+
+}
